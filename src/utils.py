06783dd9506cf5e111a70c68ffddedabcab525a6
Anton Ershov <ant_e@mail.ru>
2024-06-22 20:43:31 +0500

    add funs get_all_expenses, get_greeting

--- src/utils.py
+++ src/utils.py
@@ -10,10 +10,13 @@ PATH_TO_DATA = os.getenv("PATH_TO_DATA")
 
 def get_data_from_excel(path: str) -> pd.DataFrame:
     """Читает данные и excel-файла и возвращает DataFrame."""
-
-    result = pd.read_excel(path).to_json(orient="records", indent=4, force_ascii=False)
-    return result
-
-
-if __name__ == '__main__':
-    print(get_data_from_excel(os.path.join(PATH_TO_DATA, 'operations.xlsx')))
+    if os.path.isfile(path):
+        try:
+            result = pd.read_excel(path)
+            return result
+        except Exception as ex:
+            print(ex)
+            return []
+    else:
+        print("Файл не найден.")
+        return []
---
+++ src/views.py
@@ -0,0 +1,56 @@
+import os
+
+import pandas as pd
+
+from dotenv import load_dotenv
+
+from src.utils import get_data_from_excel
+
+load_dotenv()
+PATH_TO_DATA = os.getenv("PATH_TO_DATA")
+
+
+def get_greeting(date: str) -> str:
+    """ Принимает строку с датой и временем в формате (2024-01-01 00:00:00),
+    возвращает приветствие: «Доброе утро» / «Добрый день» / «Добрый вечер» / «Доброй ночи»"""
+    hour = int(date.split()[1].split(':')[0])
+    greeting = ''
+    if hour in range(0, 6):
+        greeting = 'Доброй ночи'
+    elif hour in range(6, 12):
+        greeting = 'Доброе утро'
+    elif hour in range(12, 18):
+        greeting = 'Добрый день'
+    elif hour in range(18, 24):
+        greeting = 'Добрый вечер'
+    return greeting
+
+
+def get_all_expenses(df: pd.DataFrame) -> list[dict]:
+    """ Принимает DataFrame с банковскими операциями, возвращает список словарей,
+    подсчитывает все расходы по карте и кешбэк"""
+    if df.empty:
+        return []
+    expenses = df.loc[df['Сумма платежа'] < 0].groupby('Номер карты').agg({'Сумма операции': 'sum'})
+    result = []
+    dict_data = expenses.to_dict().get('Сумма операции')
+    for key, item in dict_data.items():
+        result.append({'last_digits': key[-4:],
+                       'total_spent': item * -1,
+                       'cashback': round(item / -100, 2)
+                       })
+    return result
+
+
+def get_top_transactions(df: pd.DataFrame) -> list[dict]:
+    """ Принимает DataFrame с банковскими операциями, возвращает список словарей,
+    top 5 транзакций"""
+    pass
+
+
+if __name__ == '__main__':
+
+    # df = get_data_from_excel(os.path.join(PATH_TO_DATA, 'empty.xlsx'))
+    df = get_data_from_excel(os.path.join(PATH_TO_DATA, 'operations.xlsx'))
+    list_dict = get_top_transactions(df)
+    print(list_dict)
